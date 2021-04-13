5a6b005197c4c72edebdd5283aec2ce19f32b72e
Ahmad <benos.tarabain@gmail.com>
2021-04-14 02:30:58 +0300

    done, not mobile responsive though

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const row = document.querySelectorAll(".row");
+const textbox = document.querySelectorAll(".text-box");
 
 row.forEach((item) => {
   item.addEventListener("click", () => {
     console.log("clicked!");
+  });
+});
+
+textbox.forEach((item, index, nodes) => {
+  item.addEventListener("click", () => {
+    nodes.forEach((node) =>
+      node != nodes[index]
+        ? node.classList.remove("active")
+        : item.classList.toggle("active")
+    );
   });
 });
